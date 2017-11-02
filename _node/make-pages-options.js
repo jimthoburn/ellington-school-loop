8b47c6170b71a1075fb122766e97e7b92272bba8
Jim Thoburn <jim@jimthoburn.com>
2017-11-02 14:08:38 -0700

    Switch to local CSS, for testing

--- _node/make-pages-options.js
+++ _node/make-pages-options.js
@@ -20,10 +20,10 @@ exports.HEAD_ELEMENT_HTML = `
 </script>
 
 <link href="https://fonts.googleapis.com/css?family=Merriweather:400,400i,700,700i|Source+Sans+Pro:400,400i,700,700i" rel="stylesheet" />
-<link rel="stylesheet" href="http://jimthoburn.github.io/ellington-custom-code/assets/shared.css" />
-<link rel="stylesheet" href="http://jimthoburn.github.io/ellington-custom-code/assets/custom.css">
+<link rel="stylesheet" href="/custom-code/assets/shared.css" />
+<link rel="stylesheet" href="/custom-code/assets/custom.css">
 
-<script src="http://jimthoburn.github.io/ellington-custom-code/assets/shared.js"></script>
+<script src="/custom-code/assets/shared.js"></script>
 `;
 
 /*
--- custom-code/assets/shared.js
+++ custom-code/assets/shared.js
@@ -64,6 +64,17 @@
   (function() {
     try {
       document.documentElement.setAttribute('data-azusa-uri', window.location.pathname);
+
+      // Check to see if the page HTML matches a certain pattern (the HTML that we expect to see on School Loop 1.0)
+      // If it does match, look for an image that appears right after the main headline
+      // If that image is found, update it’s URL from w700 to w2000
+      document.addEventListener('DOMContentLoaded', function() {
+        var image = document.querySelector('#page_title + #block_wide_main > .block_content_main > div > div[style="width: 100%; width: 700px"] > img[width="700"]');
+        if (image) {
+          image.setAttribute('src', image.getAttribute('src').replace('w700', 'w2000'));          
+        }
+      });
+
     } catch(e) {
       if (console && console.error) console.error(e);
     }
